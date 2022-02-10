b58751f6caa53b02cea2ca254b8fe8769a8dcb00
Richard9703 <richardpaulov@gmail.com>
2022-02-10 11:24:43 +0000

    added start and shwo tries

--- run.py
+++ run.py
@@ -7,8 +7,47 @@ from words import words_list # Import words list from words file (https://www.ra
 
 def get_random_word(): # chooses a random word from the list
     word = random.choice(words_list)
-    while '-'in word or ' ' in word: # Finds a random word even when encountering a ' ' or '-' in the list
-        word = random.choice(words_list)
+    return word
 
-    return word    
+def start(word):
+	to_complete_word = "_" * len(word)
+	guessed = False
+	used_letters = []
+	used_words = []
+	attempt = 6
+	print("Start guessing!")
+	print(show_aatempts(attempts))
+	print(to_complete_word)
+	print("\n")
+	while not guessed and attempts > 0:
+		guess = input("Guess a letter / word: ").upper()
+		if len(guess) == 1 and guess.isalpha():
+
+def show_attempts(attempts):
+	tries = [
+		"""
+		Try again x1
+		""",
+		"""
+		Try again x2
+		""",
+		"""
+		Try again x3
+		""",
+		"""
+		Try again x4 
+		""",
+		"""
+		Try again x5 
+		""",
+		"""
+		Try again x6
+		""",
+	]
+	return tries[attempts]
+
+
+
+
+
 
