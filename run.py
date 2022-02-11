bf6c45b430354412c3547dc27c5d603291f8bd20
Richard9703 <richardpaulov@gmail.com>
2022-02-11 12:29:53 +0000

    added user input

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
-from words import words_list 	# Import words list from words file (https://www.randomlists.com/data/words.json)
+from words import words 	# Import words list from words file (https://www.randomlists.com/data/words.json)
 
 print("Play Hangman!")
 print("You have 6 lives")
@@ -10,19 +10,31 @@ print("You have 6 lives")
 
 # Get words from list
 def get_random_word():
-    word = random.choice(words_list).upper()
-    return random.choice(word).upper()
+    word = random.choice(words).upper()
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
 
-letter = get_random_word()
+    return word
 
-def main():
-    num_of_attempts = 0
-    guesses = []
-    guessed = False
-    word_to_guess = '' * len(letter)
-    print('Guess the word \"', word_to_guess , '\" It is ', len(word_to_guess))
+def start():
+    word = get_random_word(words)
+    num_letters_word = set(word)
+    letters = set(string.ascii_uppercase)
+    letters_used = set()
+
+    user_letter = input('Guess a letter').upper()
+    if user_letter in letter - letters_used:
+        letters_used.add(user_letter)
+        if user_letter in num_letters_word:
+            num_letters_word.remove(user_letter)
+
+    elif user_letter in letters_used:
+        print('You have used this letter...Try again.')
 
 
+
+user_input = input('Types a word: ')
+print(user_input)
 
 
 
