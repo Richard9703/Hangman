da8bfba9811b2ca7c2c028c490103e422da54812
Richard9703 <richardpaulov@gmail.com>
2022-02-12 07:51:47 +0000

    Added get_word from imported list

--- run.py
+++ run.py
@@ -8,35 +8,12 @@ print("Play Hangman!")
 print("You have 6 lives")
 
 
-# Get words from list
-def get_random_word():
-    word = random.choice(words).upper()
-    while '-' in word or ' ' in word:
-        word = random.choice(words)
+# Get words from words list
+get_word = random.choice(words)
 
-    return word
-
-def start():
-    word = get_random_word(words)
-    num_letters_word = set(word)
-    letters = set(string.ascii_uppercase)
-    letters_used = set()
-
-    user_letter = input('Guess a letter').upper()
-    if user_letter in letter - letters_used:
-        letters_used.add(user_letter)
-        if user_letter in num_letters_word:
-            num_letters_word.remove(user_letter)
-    elif user_letter in letters_used:
-        print('You have used this letter...Try again.')
-    else: 
-        print('Invalid character...Try again.')
-
-
-
-user_input = input('Types a word: ')
-print(user_input)
-
+# Print letters in '_'
+for words in get_word:
+    print('_', end=" ")
 
 
 
