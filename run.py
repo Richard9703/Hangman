ce96ca3a84c34a2c0e345265e839133a968d175a
Richard9703 <richardpaulov@gmail.com>
2022-02-13 08:45:52 +0000

    while loop to display attempts and letters used

--- run.py
+++ run.py
@@ -6,20 +6,28 @@ import string
 from words import words 	# Import words list from words file (https://www.randomlists.com/data/words.json)
 
 
-
 def get_word(words):
 	word = random.choice(words)
 	return word.upper()
 
 def hangman_game():
+	word = get_word(words)
 	letters = string.ascii_uppercase
 	letters_in_word = set(word)
 	letters_used = set()
 	attempts = 6
 
-	user_input = input('Guess the word: ').upper()
-	if user_input in letters_in_word - letters_used:
-		letters_used.add(user_input)
-		if user_input in letters_in_word:
-			letters_in_word.remove(user_input)
-			print(' ')
+
+	while len(letters_in_word) > 0 and attempts > 0:
+		print(attempts, 'attempts left')
+		print('You have used: ', ' '.join(letters_used))
+
+
+
+		user_input = input('Guess the word: ').upper()
+		if user_input in letters_in_word - letters_used:
+			letters_used.add(user_input)
+			if user_input in letters_in_word:
+				letters_in_word.remove(user_input)
+				print(' ')
+
