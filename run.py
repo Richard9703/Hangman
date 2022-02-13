50b1996d92225675dd2e1e31bc2ea789e23011cd
Richard9703 <richardpaulov@gmail.com>
2022-02-13 09:58:53 +0000

    added hangman game function

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
-import string
 from words import words 	# Import words list from words file (https://www.randomlists.com/data/words.json)
 
 
@@ -10,24 +9,28 @@ def get_word(words):
 	word = random.choice(words)
 	return word.upper()
 
-def hangman_game():
-	word = get_word(words)
-	letters = string.ascii_uppercase
-	letters_in_word = set(word)
-	letters_used = set()
-	attempts = 6
 
 
-	while len(letters_in_word) > 0 and attempts > 0:
-		print(attempts, 'attempts left')
-		print('You have used: ', ' '.join(letters_used))
+user_guess = True
+empty_word = "_" * len(get_word(words))
+lives = 0
+while user_guess and lives < 5:
+	user_input = input("Guess the word: ")
+	if user_input == get_word(words):
+		word_to_guess = get_word(words).find(user_input)
+		empty_word = empty_word[:word_to_guess] + user_guess + empty_word[word_to_guess+1:]
+	else:
+		lives += 1
+		print(f"You have {lives} left")
+	print(empty_word)
+	if "_" != empty_word:
+		print("You are correct!")
+		user_guess = False
+	if lives == 5:
+		print("Try again!")				
 
 
+get_word(words)
 
-		user_input = input('Guess the word: ').upper()
-		if user_input in letters_in_word - letters_used:
-			letters_used.add(user_input)
-			if user_input in letters_in_word:
-				letters_in_word.remove(user_input)
-				print(' ')
 
+	
