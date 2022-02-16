b69b10a887fd923fb24c29aa9e8be78e2dafd97e
Richard9703 <richardpaulov@gmail.com>
2022-02-16 03:18:50 +0000

    fixed issue with game not exiting

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-import random
+import random, sys
 
 
 stages = ['''
 
-  
+
 
        +---+
 
@@ -19,23 +19,7 @@ stages = ['''
 
      =========''', '''
 
-    
 
-       +---+
-
-       |   |
-
-       O   |
-
-           |
-
-           |
-
-           |
-
-     =========''', '''
-
-    
 
        +---+
 
@@ -43,6 +27,22 @@ stages = ['''
 
        O   |
 
+           |
+
+           |
+
+           |
+
+     =========''', '''
+
+
+
+       +---+
+
+       |   |
+
+       O   |
+
        |   |
 
            |
@@ -51,7 +51,7 @@ stages = ['''
 
      =========''', '''
 
-    
+
 
        +---+
 
@@ -67,7 +67,7 @@ stages = ['''
 
      =========''', '''
 
-    
+
 
        +---+
 
@@ -83,7 +83,7 @@ stages = ['''
 
      =========''', '''
 
-    
+
 
        +---+
 
@@ -99,7 +99,7 @@ stages = ['''
 
      =========''', '''
 
-    
+
 
        +---+
 
@@ -119,96 +119,91 @@ words = ["avocado","chicken","funds","bankrupt","cushion","filming","apartment",
 
 
 def get_word(word_list):
-	word_index = random.randint(0,len(word_list) - 1)
-	return word_list[word_index]
+    word_index = random.randint(0, (len(word_list) - 1))
+    return word_list[word_index]
 
 
 def hangman_board(stages, correct_letters, missed_letters, hidden_word):
-	print(stages[7 - lives])
-	print()
+    print(stages[6 - lives])
+    print("You have " + str(lives) + " lives remaining")
+    
 
-	print("Missed letters:", end="")
-	for letter in missed_letters:
-		print(letter, end="")
-	print()	
+    print("Missed letters:", end=" ")
+    for letter in missed_letters:
+        print(letter, end=" ")
+   
 
-	blank = "_" * len(hidden_word)
+    blank = "_" * len(hidden_word)
 
-	for i in range(len(hidden_word)):
-		if hidden_word[i] in correct_letters:
-			blank = blank[:i] + hidden_word[i] + blank[i+1:]
+    for i in range(len(hidden_word)):
+        if hidden_word[i] in correct_letters:
+            blank = blank[:i] + hidden_word[i] + blank[i+1:]
 
-	for letter in blank:
-		print(letter, end="")
-	print()	
-
+    for letter in blank:
+        print(letter, end=" ")
+    
 
 def get_guess(guessed):
-	while True:
-		print("Guess a letter: ")
-		guess = input()
-		guess = guess.lower()
-		if len(guess) != 1:
-			print("Only one letter at a time")
-		elif guess in guessed:
-			print("Letter used, use another!")
-		elif guess not in 'abcdefghijklmnopqrstuvwxyz':
-			print("Enter a letter:")
-		else:
-			return guess
+    while True:
+        print("\nGuess a letter: ")
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print("Only one letter at a time")
+        elif guess in guessed:
+            print("Letter used, use another!")
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print("Enter a letter:")
+        else:
+            return guess
 
 
 def try_again(): # Allows the user to play again after finishing the game
-   	print("Play again? (yes or no)")
-   	return input().lower().startswith("y")
+       print("Play again? (yes or no)")
+       return input().lower().startswith("y")
 
 
 print("HANGMAN")
-lives = 7
-missed_letters =""
+lives = 6
+missed_letters = ""
 correct_letters = ""
 hidden_word = get_word(words)
 finished = False
 
-while True:
-	hangman_board(stages, correct_letters, missed_letters, hidden_word)
+while True:    
+    hangman_board(stages, correct_letters, missed_letters, hidden_word)
 
-	guess = get_guess(missed_letters + correct_letters) # Type in a letter
+    guess = get_guess(missed_letters + correct_letters) # Type in a letter
 
-	if guess in hidden_word:
-		correct_letters = correct_letters + guess
+    if guess in hidden_word:
+        correct_letters = correct_letters + guess
 
     # checks win
-	all_letters = True
-	for i in range(len(hidden_word)):
-		print("test1")
-		if hidden_word[i] not in correct_letters:
-			print("test2")
-			lives -= 1
-			all_letters = False
-			break
-	if all_letters:
-		print("test3")
-		print("Hidden word is " + hidden_word + "! You win!")  
-		finished = True 
-	else:
-		print("test4")
-		missed_letters = missed_letters + guess   
-		
+        all_letters = True
+        for i in range(len(hidden_word)):
+           
+            if hidden_word[i] not in correct_letters:
+                
+                all_letters = False
+                break
+        if all_letters:
+            
+            print("Hidden word is " + hidden_word + "! You win!")
+            finished = True 
+    else:
+        
+        missed_letters = missed_letters + guess
+        lives -= 1
 
-	if len(missed_letters) == len(stages) - 1:
-		hangman_board(stages, correct_letters, missed_letters, hidden_word)
-		print("No more lives!\n" + str(len(missed_letters)) + "missed letters and " + str(len(correct_letters)) + "correct letters, the word was " + hidden_word + "")
-		finsished = True
-
-	
-	if finished or lives == 0:
-		if try_again():
-			missed_letters = ""
-			correct_letters = ""
-			finish = False 
-			hidden_word = get_word(words)
-		else:
-			break  
-
-
+        if lives == 0:
+            hangman_board(stages, correct_letters, missed_letters, hidden_word)
+            print("No more lives!\n" + str(len(missed_letters)) + " missed letters and " + str(len(correct_letters)) + " correct letters.\nThe word was " + hidden_word + "\n")
+            finsished = True
+            if try_again():
+                missed_letters = ""
+                correct_letters = ""
+                finished = False
+                lives = 6
+                hidden_word = get_word(words)
+            else:
+                sys.exit()
